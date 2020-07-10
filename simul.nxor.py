bfa76f2e7794eeb2d45f795285ca2798b8646020
David Lubicz <david.lubicz@univ-rennes1.fr>
2020-07-10 17:36:34 +0200

    added temp

--- simul.nxor.py
+++ simul.nxor.py
@@ -15,4 +15,4 @@ ratio_slope = [1]
 facteur = 5.27/2*10**-7
 ratio_slope = [facteur * i for i in ratio_slope]
 
-print find_waiting_time(alpha, f, memory, 14, ratio_slope, [12000, 14000], 0.997, 0.001, True)
+print find_waiting_time([alpha], f, memory, 14, ratio_slope, [12000, 14000], 0.997, 0.001, True)
